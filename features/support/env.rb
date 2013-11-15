d819dbf7e162ad59ee05835e7b6486c6c67e8428
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2013-11-15 16:22:26 +0100

    Generate shims

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,4 +2,19 @@ require "cucumber"
 require "aruba/cucumber"
 
 # Add shims to PATH
-ENV['PATH'] = "#{Dir.pwd}/features/shims#{File::PATH_SEPARATOR}#{ENV['PATH']}"
+SHIMS = "#{Dir.pwd}/tmp/aruba/shims"
+ENV['PATH'] = "#{SHIMS}:#{ENV['PATH']}"
+
+def generate_shims_for(commands)
+  FileUtils.mkdir_p(SHIMS)
+  commands.each do |cmd|
+    File.open("#{SHIMS}/#{cmd}", "w", 0755) do |f|
+      f << "#!/bin/sh\n"
+      f << "echo \"#{cmd} $@\"\n"
+    end
+  end
+end
+
+Before do
+  generate_shims_for %w(berks rsync ssh vagrant)
+end
